a85d8767b77f74c5c922d72dad155d5a3d263d44
hellboy95 <ajaypal95@gmail.com>
2020-04-07 20:15:55 -0700

    Wokring image slider with image gradient :)

---
+++ src/components/Home/Slider.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useStaticQuery, graphql } from "gatsby"
+
+import BackgroundSlider from 'gatsby-image-background-slider';
+
+export default function Slider() {
+    return (
+      <BackgroundSlider 
+      query={img}
+      initDelay={2} // delay before the first transition (if left at 0, the first image will be skipped initially)
+      transition={4} // transition duration between images
+      duration={8} // how long an image is shown
+      // specify images to include (and their order) according to `relativePath`
+      images={["preethi.jpeg", "butterfly.png"]} 
+
+      // pass down standard element props
+      style={{
+        transform: "rotate(-2deg) scale(.9)",
+      }}           
+    > 
+      {/* Captions in sync with background images*/}
+      <div>Woof</div>
+      <div>Meow</div>
+      
+    </BackgroundSlider>
+        
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,65 @@
 import React from "react"
-import { Link ,graphql} from "gatsby"
+import { Link ,graphql, useStaticQuery} from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import Background from "../components/global/Background"
 import Info from "../components/Home/Info"
-
+import Slider from "../components/Home/Slider"
+import BackgroundSlider from "gatsby-image-background-slider"
 
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <Background className="col-10 col-sm-10" img={data.img.childImageSharp.fluid} styleClass="default-background gradient:after">
+    {/* <Background className="col-10 col-sm-10" img={data.img.childImageSharp.fluid} styleClass="default-background gradient:after">
       <div className="gradient">
       Avinash Industries
       </div>
         
-      </Background>
-    <Info>
-    </Info>
+      </Background> */}
+      <div className="default-background">
+          <div className="row">
+          
+                  <BackgroundSlider 
+                    query = {useStaticQuery(graphql`
+                                query{
+                                  backgrounds: allFile(filter: {sourceInstanceName: {eq: "images"}}) {
+                                    nodes{
+                                      relativePath
+                                      childImageSharp {
+                                        fluid(maxWidth: 2000, quality: 100) {
+                                          ...GatsbyImageSharpFluid_withWebp
+                                        }
+                                      }
+                                    }
+                                  }
+                                }
+                              `)}
+                    initDelay={2}
+                    transition={4}
+                    duration={8}
+                    images={["preethi.jpeg", "butterfly.png"]}
+                    style={{
+                      top: "105px",
+                      backgroundSize: "cover",
+                      backgroundPosition: "center"
+                    }}  />
+                    <h1 className="title text-white text-center text-uppercase display-4 font-weight-bold">Avinash Industries</h1>
+     
+          </div>
+     
+      </div>
+      
+
+    <Info/> 
+    
   </Layout>
 
   
 )
 
-export const query = graphql`
+/*export const query = graphql`
 {
   img: file(relativePath: {eq: "preethi.jpeg"}) {
     childImageSharp {
@@ -34,5 +69,22 @@ export const query = graphql`
     }
   }
 }
-`;
+`; */
+
+/*export const query = graphql`
+{
+  backgrounds: allFile(filter: {sourceInstanceName: {eq: "images"}}) {
+     edges{
+      node {
+       relativePath
+        childImageSharp {
+          fluid{
+            ...GatsbyImageSharpFluid
+          }
+       }
+     }
+    }
+  }
+}`;*/
+
 export default IndexPage
