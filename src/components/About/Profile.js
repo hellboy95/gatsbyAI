132ea3c39cd356181d2b060e9ebe9ec6be06a398
hellboy95 <ajaypal95@gmail.com>
2020-04-16 01:10:23 -0700

    About Page Layout Complete

---
+++ src/components/About/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from "gatsby"
+import Title from "../global/Title"
+
+export default function Profile() {
+    return (
+        <section className="test py-5">
+            <div className="container">
+               <Title title="Company Profile"/>
+                <div className="row">
+                    <div className="col-10 col-sm-8 mx-auto">
+                        <p className="text-dark">
+                        Promoted by Shri K.A.Palaniappan and his three sons in 1985 at Tiruchirapalli, we started manufacturing metal articles for domestic & industrial kitchens, large canteens, worship places, government free schemes, etc.,.
+
+By 1992, the business moved to Chennai, to become contract manufactures of original components of kitchen appliances for the leading brands of South India, namely, Butterfly, Premier, Prestige, Preethi, etc.,.
+
+By 2000, the business ventured into contract manufacturing the finished kitchen hobs (LPG Stoves) to major brand owners. By 2008, the business ventured into national market, by setting up the plant near Baddi, Himachal Pradesh to leverage the tax holiday schemes by the central government.
+
+Backed with more than 3 decades of manufacturing experience, customer friendly and ethical business policies, the promoters have immense confidence to get this business to the top line ranks nationally. 
+
+The team has great appetite to take up challenging projects and deliver services on –time, every time and still be very competitive on pricing without compromising on quality and promises. We have been really successful on high volume, short lead-time and still competitive orders entrusted on us.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,49 +5,46 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 
+import Profile from "../components/About/Profile"
 import Info from "../components/Home/Info"
-
-import BackgroundSlider from "gatsby-image-background-slider"
 import Characteristics from "../components/Home/Characteristics"
-import Test from "../components/Home/Test"
+
 
 const AboutPage = ({data}) => (
   <Layout>
     <SEO title="About" />
-      <div className="default-background">
-              <BackgroundSlider 
-                    query = {data}
-                    initDelay={2}
-                    transition={4}
-                    duration={5}
-                    images={["kitchen1.jpg","bg1.jpg","bg2.jpg","kitchen.jpeg","preethi.jpeg", "butterfly.jpg"]}
-                    style={{
-                      top: "105px",
-                      maxHeight: "100vh",
-                      backgroundSize: "cover",
-                      backgroundPosition: "center"
-                    }}  />
-                <div>
-                    <h1 className="heroheader text-uppercase display-4 font-weight-bold mt-5 ">Avinash Industries</h1>
+    <div className="mainContentArea">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-3 leftcol" id="leftcol">
+                    <div className="sticky-top leftSticky">
+                    <ul>
+                        <li><a href="#profile">Company Profile</a></li>
+                        <li><a href="#Team">Management Team</a></li>
+                    </ul>
+                    </div>
+                </div>
+                <div className="col-md-9" id="rightcol">
+                    <Profile id="profile"/>
+                    <Characteristics que={data} />
+                    <Info id="Team"/>
+                    <Info />
+                    <Info  />
+                    <Info />
+                    <Info  />
+
 
                 </div>
-                <div>
-                    <p className="col-10 col-sm-8 mx-auto text-center eggwhite" > We are Original Equipment Manufacturers (OEM) building high-quality products for the leading kitchen appliance brands in the nation.</p>
-              
-                </div>
-                      
-                     
-          </div>
-
-      
-    <Characteristics que={data}/>
-    <Info/> 
+            </div>
+        </div>
+    </div>
     
-    <Test que={data}/>
   </Layout>
 
   
 )
+
+
 
 export const query = graphql`
 {
