5d3cddeb2d51b02d6d2d8aa0dd1247288760a5ef
hellboy95 <ajaypal95@gmail.com>
2020-04-23 14:13:00 -0700

    nav about stikctop

--- src/components/About/NavbarAbout.js
+++ src/components/About/NavbarAbout.js
@@ -3,12 +3,12 @@ import Img from "gatsby-image"
 
 
 
-export default function NavbarAbout({que}) {
+export default function NavbarAbout({id1,id2,id3}) {
     return (
-            <div className="container-fluid mx-auto py-3 navabout">
+            <div className="container-fluid sticky-top mx-auto py-3 ">
                     <div className="row mx-auto ">
                         <div className="col-4 text-center">
-                            <a href="#profile" ><span>Company Profile</span></a>
+                            <a href={id1} ><span>Company Profile</span></a>
                         </div>
                         <div className="col-4 text-center">
                             <a href="#mission" ><span>Our Mission & Vision</span></a>
